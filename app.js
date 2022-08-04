c9724a6acfe823ae41d55f8ced40e9da9173b49a
DarkVoid01 <arnoldeny15@gmail.com>
2022-08-04 13:14:54 +0100

    made some crazy changes that will make me start again

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-const express = require('express')
-const app = express()
+
 // require('dotenv').config()
-const cors = require('cors') //allows the end points to be used across the browser
-const route = require('./api/routes/userRoute.js')
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-require('./api/db/db').connect()
+ //allows the end points to be used across the browser
 
 
 
-app.get('/', (req, res) => {
-    return res.json({
-        status: "Success",
-        message: "Welcome to CRUDApp"
-    })
-})
-app.use('/api/v1', route)
 
-app.use(cors())
-module.exports = app
+
+
+// app.get('/', (req, res) => {
+//     return res.json({
+//         status: "Success",
+//         message: "Welcome to CRUDApp"
+//     })
+// })
+
+// module.exports = app
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const app = require('./app.js')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const route = require('./api/routes/userRoute.js')
 require('dotenv').config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+require('./api/db/db').connect()
+app.use('/api/v1', route)
+
+app.use(cors())
 const port = process.env.PORT || 5000
 
 
